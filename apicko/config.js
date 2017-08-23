e0a1e390a1ce6022f8ac23ac930eaca23bb96ed9
Martin Fouček <martin.foucek@socialbakers.com>
2017-08-23 18:04:57 +0200

    API is just one part of the project

--- apicko/config.js
+++ apicko/config.js
@@ -1,4 +1,5 @@
-require('dotenv').config({path: `${__dirname}/.env`})
+const configPath = require('path').resolve(__dirname, '../.env')
+require('dotenv').config({path: configPath})
 
 for (let key of ['NAME', 'PASS', 'PORT']) {
 	if (key in process.env) continue
--- apicko/index.js
+++ apicko/index.js
@@ -1,13 +1,13 @@
 const express = require('express')
-const idos = require('./lib/idos')
+const idos = require('./idos')
 const config = require('./config')
 
 const app = express()
 
 idos.createClient(config.url, config.credentials)
 .then(idosClient => {
-	const middleware = require('./lib/middleware')(idosClient)
-	const endpoints = require('./lib/endpoints')(idosClient)
+	const middleware = require('./middleware')(idosClient)
+	const endpoints = require('./endpoints')(idosClient)
 	app.use('/api', middleware.renewSession)
 	app.get('/api/search', endpoints.getDepartures)
 	app.use(middleware.handleError)
